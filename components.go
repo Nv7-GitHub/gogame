9e93ebb714b2fa4429fc9dad62892c21f84ad860
Nv7 <junk4Nv7@gmail.com>
2020-08-14 15:03:19 -0700

    Rect now fully implemented

--- components.go
+++ components.go
@@ -11,29 +11,41 @@ type Component interface {
 
 // Rect is a rectangle, it draws a rectangle to the screen
 type Rect struct {
-	x      int32
-	y      int32
-	width  int32
-	height int32
-	color  Color
+	X      int32
+	Y      int32
+	Width  int32
+	Height int32
+	Color  Color
 }
 
 // Load function is called on initialization, but is mostly for things that read from disk
-func (r Rect) Load() {
+func (r *Rect) Load() {
 	return
 }
 
 // Draw is for drawing rect
-func (r Rect) Draw() {
-	rl.DrawRectangle(r.x, r.y, r.width, r.height, r.color.color)
+func (r *Rect) Draw() {
+	rl.DrawRectangle(r.X, r.Y, r.Width, r.Height, r.Color.color)
 }
 
 // Unload is for taking things from disk out of RAM, doesnt apply to rect
-func (r Rect) Unload() {
+func (r *Rect) Unload() {
 	return
 }
 
+// Move is for moving a rectangle to a specific point
+func (r *Rect) Move(x, y int32) {
+	r.X = x
+	r.Y = y
+}
+
+// Translate is for translating a rectangle a certain amount
+func (r *Rect) Translate(changeX, changeY int32) {
+	r.X += changeX
+	r.Y += changeY
+}
+
 // NewRect creates a rect with the parameters
-func NewRect(x, y, width, height int32, color Color) Rect {
-	return Rect{x, y, width, height, color}
+func NewRect(x, y, width, height int32, color Color) *Rect {
+	return &Rect{x, y, width, height, color}
 }
