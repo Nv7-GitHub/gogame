8845df4d667d4b5e967e928e351acdec417b47da
Nv7 <junk4Nv7@gmail.com>
2020-08-14 15:41:08 -0700

    Added image primitive

--- primitives.go
+++ primitives.go
@@ -84,3 +84,54 @@ func (c *Circle) Translate(changeX, changeY int32) {
 func NewCircle(x, y int32, radius float32, color Color) *Circle {
 	return &Circle{x, y, radius, color}
 }
+
+// Image is a image, it draws an image to the screen
+type Image struct {
+	X     int32
+	Y     int32
+	Path  string
+	tex   rl.Texture2D
+	color rl.Color
+	image *rl.Image
+}
+
+// Load function is called on initialization, but is mostly for things that read from disk
+func (i *Image) Load() {
+	i.image = rl.LoadImage(i.Path)
+	i.tex = rl.LoadTextureFromImage(i.image)
+}
+
+// Draw is for drawing oval
+func (i *Image) Draw() {
+	rl.DrawTexture(i.tex, i.X, i.Y, i.color)
+}
+
+// Unload is for taking things from disk out of RAM, doesnt apply to rect
+func (i *Image) Unload() {
+	rl.UnloadImage(i.image)
+}
+
+// Resize is for resizing an image
+func (i *Image) Resize(width, height int32) {
+	rl.ImageResize(i.image, width, height)
+	i.tex = rl.LoadTextureFromImage(i.image)
+}
+
+// Move is for moving a rectangle to a specific point
+func (i *Image) Move(x, y int32) {
+	i.X = x
+	i.Y = y
+}
+
+// Translate is for translating a rectangle a certain amount
+func (i *Image) Translate(changeX, changeY int32) {
+	i.X += changeX
+	i.Y += changeY
+}
+
+// NewImage creates a rect with the parameters
+func NewImage(x, y, width, height int32, path string) *Image {
+	var img *rl.Image
+	var tex *rl.Texture2D
+	return &Image{x, y, path, *tex, rl.RayWhite, img}
+}
