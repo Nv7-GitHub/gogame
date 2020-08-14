ad2ad564dbc56bec6575d66cb65681a283948caa
Nv7 <junk4Nv7@gmail.com>
2020-08-14 15:20:54 -0700

    Added primitives.go, put all primitives there

--- primitives.go
+++ primitives.go
@@ -44,3 +44,43 @@ func (r *Rect) Translate(changeX, changeY int32) {
 func NewRect(x, y, width, height int32, color Color) *Rect {
 	return &Rect{x, y, width, height, color}
 }
+
+// Circle is a circle, it draws a circle to the screen
+type Circle struct {
+	X      int32
+	Y      int32
+	Radius float32
+	Color  Color
+}
+
+// Load function is called on initialization, but is mostly for things that read from disk
+func (c *Circle) Load() {
+	return
+}
+
+// Draw is for drawing rect
+func (c *Circle) Draw() {
+	rl.DrawCircle(c.X, c.Y, c.Radius, c.Color.Color)
+}
+
+// Unload is for taking things from disk out of RAM, doesnt apply to rect
+func (c *Circle) Unload() {
+	return
+}
+
+// Move is for moving a circle to a specific point
+func (c *Circle) Move(x, y int32) {
+	c.X = x
+	c.Y = y
+}
+
+// Translate is for translating a circle a certain amount
+func (c *Circle) Translate(changeX, changeY int32) {
+	c.X += changeX
+	c.Y += changeY
+}
+
+// NewCircle creates a rect with the parameters
+func NewCircle(x, y int32, radius float32, color Color) *Circle {
+	return &Circle{x, y, radius, color}
+}
